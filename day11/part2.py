bdc930a930e1ef28f50680698a20012e83a49d56
Suhas Bharadwaj <suhas.five@gmail.com>
2021-12-25 01:52:59 +0530

    Done with day 11 part 2

--- day11/part2.py
+++ day11/part2.py
@@ -1,4 +1,24 @@
 import day11.helpers as helpers
 
-def program(input_data):
-    return 0
+def program(dumbo_grid: list[list[int]]):
+    current_step = 0
+    are_all_zeroes = False
+
+    while (not are_all_zeroes):
+        about_to_blow = helpers.step_one(dumbo_grid)
+        helpers.step_two(dumbo_grid, about_to_blow)
+        helpers.step_three(dumbo_grid)
+        are_all_zeroes = is_zero_grid(dumbo_grid)
+        current_step += 1
+    
+    return current_step
+
+def is_zero_grid(dumbo_grid: list[list[int]]) -> bool:
+    total_dumbos = len(dumbo_grid) * len(dumbo_grid[0])
+    zero_dumbos = 0
+    for i in range(len(dumbo_grid)):
+        for j in range(len(dumbo_grid[i])):
+            if dumbo_grid[i][j] == 0:
+                zero_dumbos += 1
+    
+    return zero_dumbos == total_dumbos
