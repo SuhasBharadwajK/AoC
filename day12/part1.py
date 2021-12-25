27a61e1e04766600d5b5bbffa36d044c30df37f8
Suhas Bharadwaj <suhas.five@gmail.com>
2021-12-25 20:19:00 +0530

    Refactored day 12

--- day12/part1.py
+++ day12/part1.py
@@ -1,7 +1,25 @@
 import day12.helpers as helpers
+import copy
 
 def program(cave_paths):
     path_count = 0
     graph = helpers.construct_graph(cave_paths)
-    path_count = helpers.trace_paths(graph, helpers.start_cave)
+    path_count = trace_paths(graph, helpers.start_cave)
     return path_count
+
+def trace_paths(graph: dict[str, list[str]], source_cave: str, path: list[str] = [], counter = 0):
+    
+    if source_cave.islower() and source_cave in path:
+        return counter
+
+    path.append(source_cave)
+
+    if source_cave == helpers.end_cave:
+        return counter + 1
+
+    links = graph[source_cave]
+    for link in links:
+        if link != helpers.start_cave:
+            counter = trace_paths(graph, link, copy.deepcopy(path), counter)
+
+    return counter
