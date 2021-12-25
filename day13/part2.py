40d6e47b4df34bcc62181eaa20b8170f11d44261
Suhas Bharadwaj <suhas.five@gmail.com>
2021-12-25 22:55:18 +0530

    Done with day 13 part 2

--- day13/part2.py
+++ day13/part2.py
@@ -1,4 +1,11 @@
 import day13.helpers as helpers
 
-def program(input_data):
-    return 0
+def program(input_lines):
+    dot_pattern, fold_instructions = helpers.parse_input(input_lines)
+    resltant_pattern = helpers.fold_paper(dot_pattern, fold_instructions, len(fold_instructions))
+
+    paper = helpers.get_paper(resltant_pattern)
+    plotted_paper = helpers.plot_on_paper(paper, resltant_pattern)
+    helpers.print_paper(plotted_paper)
+    
+    return len(resltant_pattern)
