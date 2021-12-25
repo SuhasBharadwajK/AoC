ae8d72d45648223781a57f966b43c1c43a661592
Suhas Bharadwaj <suhas.five@gmail.com>
2021-12-25 20:15:11 +0530

    Done with day 12 part 2

--- day12/helpers.py
+++ day12/helpers.py
@@ -1,4 +1,5 @@
 import copy
+from os import path
 
 start_cave = "start"
 end_cave = "end"
@@ -39,3 +40,39 @@ def trace_paths(graph: dict[str, list[str]], source_cave: str, path: list[str] =
             counter = trace_paths(graph, link, copy.deepcopy(path), counter)
 
     return counter
+
+def trace_paths_with_small_caves(graph: dict[str, list[str]], source_cave: str, path: list[str] = [], counter = 0, limiter = 0):
+
+    current_cave_occurrances = len([x for x in path if x == source_cave])
+    if source_cave.islower() and is_any_small_cave_visited_twice(path) and (current_cave_occurrances == 2 or current_cave_occurrances == 1):
+        return counter
+
+    path.append(source_cave)
+
+    if source_cave == end_cave:
+        return counter + 1
+
+    links = graph[source_cave]
+    for link in links:
+        if link != start_cave:
+            counter = trace_paths_with_small_caves(graph, link, copy.deepcopy(path), counter, limiter + 1)
+
+    return counter
+
+def get_path_dictionary(path) -> dict[str:int]:
+    path_dict = {}
+    for item in path:
+        if item in path_dict:
+            path_dict[item] += 1
+        else:
+            path_dict[item] = 1
+
+    return path_dict
+
+def is_any_small_cave_visited_twice(path: list[str]) -> bool:
+    path_dict = get_path_dictionary(path)
+    for k, v in path_dict.items():
+        if v == 2 and k.islower():
+            return True
+
+    return False
--- day12/part2.py
+++ day12/part2.py
@@ -1,2 +1,6 @@
+import day12.helpers as helpers
+
 def program(cave_paths):
-    return 0
+    graph = helpers.construct_graph(cave_paths)
+    path_count = helpers.trace_paths_with_small_caves(graph, helpers.start_cave)
+    return path_count
