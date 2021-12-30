3158613e0cdd59667fe90d2b1f1a15b938d88048
Suhas Bharadwaj <suhas.five@gmail.com>
2021-12-30 17:21:21 +0530

    Done with day 14 part 2

--- day14/part1.py
+++ day14/part1.py
@@ -1,10 +1,55 @@
 import day14.helpers as helpers
-import re
 
 def program(input_lines):
     template, insertion_rules = helpers.parse_input(input_lines)
     steps_required = 10
-    polymer = helpers.get_polymer(steps_required, template, insertion_rules)
-    occurrences = helpers.get_element_occurrences(polymer)
+    polymer = get_polymer(steps_required, template, insertion_rules)
+    occurrences = get_element_occurrences(polymer)
     result = helpers.get_min_and_max_occurrence_difference(occurrences)
     return result
+
+def get_substring_indexes(source, sub):
+    indexes = []
+    for i in range(len(source) - 1):
+        if source[i : i + 2] == sub:
+            indexes.append(i)
+
+    return indexes
+
+def get_polymer(steps_required, template, insertion_rules):
+    for step in range(steps_required):
+        rule_index_map = {}
+        for rule in insertion_rules:
+            pair = rule
+            pair_match_indexes = get_substring_indexes(template, pair)
+
+            for index in pair_match_indexes:
+                rule_index_map[index] = (pair, insertion_rules[pair])
+
+        elements_inserted = []
+        last_template_index = 0
+        index = 0
+        while last_template_index < len(template):
+            elements_inserted.append(template[last_template_index])
+            last_template_index += 1
+
+            if index in rule_index_map:
+                rule = rule_index_map[index]
+                element = insertion_rules[rule[0]]
+                elements_inserted.append(element)
+
+            index += 1
+            
+        template = ''.join(elements_inserted)
+
+    return template
+
+def get_element_occurrences(polymer: str):
+    occurrence_map: dict[str:int] = {}
+    for element in polymer:
+        if element not in occurrence_map:
+            occurrence_map[element] = 0
+
+        occurrence_map[element] += 1
+        
+    return occurrence_map
--- day14/part2.py
+++ day14/part2.py
@@ -1,5 +1,59 @@
 import day14.helpers as helpers
+import copy
 
 def program(input_lines):
     template, insertion_rules = helpers.parse_input(input_lines)
-    return 0
+    template_map = get_template_map(template)
+    steps_required = 40
+    new_map = insert(template_map, insertion_rules, steps_required)
+    last = template[-1:]
+    count_map = get_count_map(new_map, last)
+    diff = helpers.get_min_and_max_occurrence_difference(count_map)
+    return diff
+
+def insert(template_map: dict[str:int], insertion_rules: dict[str:str], steps: int):
+    for step in range(steps):
+        new_map = {}
+        for pair in template_map:
+            if pair in insertion_rules:
+                element = insertion_rules[pair]
+                new_pairs = pair[0] + element, element + pair[1]
+                for new_pair in new_pairs:
+                    if new_pair not in new_map:
+                        new_map[new_pair] = 0
+
+                    new_map[new_pair] += template_map[pair]
+
+        template_map = copy.deepcopy(new_map)
+    
+    return template_map
+
+def get_count_map(template_map, last: str = None):
+    count_map = {}
+    index = 0
+    for key in template_map:
+        if key[0] not in count_map:
+            count_map[key[0]] = 0
+
+        count_map[key[0]] += template_map[key]
+        index += 1
+
+        if index == len(template_map) - 1 and last is not None:
+            if last not in count_map:
+                count_map[last] = 0
+
+            count_map[last] += 1
+    
+    return count_map
+
+def get_template_map(template):
+    template_map: dict[str:int] = {}
+    for i in range(len(template) - 1):
+        key = template[i : i + 2]
+
+        if key not in template_map:
+            template_map[key] = 0
+
+        template_map[template[i : i + 2]] += 1
+    
+    return template_map
